064f737eea7ac2059f38751dd6100bc9facca13d
steviehubble <66645797+steviehubble@users.noreply.github.com>
2020-09-22 22:24:59 +0100

    Update EmailApp.java

--- EmailApp.java
+++ EmailApp.java
@@ -3,13 +3,6 @@ import javax.swing.*;
 public class EmailApp {
 	
 	public static void main(String[] args) {
-		
-		User user1 = new User("Stevie", "Hubble", "stevie-hubble@hotmail.co.uk");
-		
-		System.out.println(user1.getEmail());
-		System.out.println(user1.getAlternativeEmail());
-		System.out.println(user1.getMailCapacity());
-		
 
 	}
 
